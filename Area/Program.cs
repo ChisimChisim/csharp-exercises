8a200e9eaa1e22f1ad154d3f5f2aedf51cb7eeff
ChisimChisim <36910475+ChisimChisim@users.noreply.github.com>
2019-01-29 13:29:59 -0600

    Add practice code

---
+++ Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = -1;
+            do
+            {
+                Console.WriteLine("Enter a radius: ");
+                r = double.Parse(Console.ReadLine());
+            } while (r < 0);
+            double result = r * r * Math.PI;
+            Console.WriteLine("The area of a circle with radius " + r + " is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
